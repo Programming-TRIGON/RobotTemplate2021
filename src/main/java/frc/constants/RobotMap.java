4e273559e9ba56e877bcb4fa867826147ff11153
Mboesn <45824003+Mboesn@users.noreply.github.com>
2021-01-05 16:03:51 +0200

    added documentation

--- src/main/java/frc/constants/RobotMap.java
+++ src/main/java/frc/constants/RobotMap.java
@@ -1,7 +1,10 @@
 package frc.constants;
 
 /**
- * Map of hardware components IDs
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
  */
 public abstract class RobotMap {
     public CAN can = new CAN();
