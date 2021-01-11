b45e8f049345f7d4bcf417c066d7465d3cd75fec
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2021-01-11 17:33:46 +0200

    Fixed according to code review

--- src/main/java/frc/robot/subsystems/MoveableSS.java
+++ src/main/java/frc/robot/subsystems/MoveableSS.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface MoveableSS extends Subsystem {
-  public void move(double distance);
+  public void move(double power);
 
   public void stop();
 }
