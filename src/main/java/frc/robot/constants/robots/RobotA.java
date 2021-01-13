dcb4a933363cc77b5fda3ecc969d9cde63428566
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2021-01-13 22:23:28 +0200

    limelight (#4)
    * Added Limelight and supporting classes
    
    * Added dependancy injection
    
    * Fixed based on code review
    
    Co-authored-by: Mboesn <45824003+Mboesn@users.noreply.github.com>

--- src/main/java/frc/robot/constants/robots/RobotA.java
+++ src/main/java/frc/robot/constants/robots/RobotA.java
@@ -9,6 +9,13 @@ public class RobotA extends RobotConstants {
 
     // TODO: Set Constants
     public RobotA() {
-
+        // Limelight Constants
+        limelightConstants.DISTANCE_CALCULATION_A_COEFFICIENT = 1;
+        limelightConstants.DISTANCE_CALCULATION_B_COEFFICIENT = 1;
+        limelightConstants.DISTANCE_CALCULATION_C_COEFFICIENT = 1;
+        limelightConstants.LIMELIGHT_ANGLE_OFFSET = 1;
+        limelightConstants.LIMELIGHT_OFFSET_X = 1;
+        limelightConstants.LIMELIGHT_OFFSET_Y = 1;
+        limelightConstants.DEFAULT_TABLE_KEY = "limelight";
     }
 }
